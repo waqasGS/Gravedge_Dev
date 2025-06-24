fd47db425f248fb3d2dea75302b4bb8ba4830464
Zoya Shahid <zoyashahid1172@gmail.com>
2025-06-24 18:09:00 +0500

    Enemy visual effect

--- Assets/Scripts/Other/StunnedTimer.cs
+++ Assets/Scripts/Other/StunnedTimer.cs
@@ -6,14 +6,19 @@ public class StunnedTimer : MonoBehaviour
 {
     public Animator _anim;
     public float stunnedDelay;
+    public GameObject particle; // Fixed spelling from "particl"
 
     public void StartStunned()
     {
         _anim.SetBool("AfterShock", true);
         Invoke(nameof(StopStunned), stunnedDelay);
+        
+        particle.SetActive(true); // Corrected usage of SetActive
     }
+
     public void StopStunned()
     {
         _anim.SetBool("AfterShock", false);
+        particle.SetActive(false); // Corrected usage of SetActive
     }
 }
