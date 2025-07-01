0ed69e877d6f2477ee779cf81283c4791c33cad4
Ali <171376812+alijgamedev@users.noreply.github.com>
2025-07-01 00:16:09 -0700

    Motorcycle Camera and UI Setup

--- Assets/Scripts/BikeUI.cs
+++ Assets/Scripts/BikeUI.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using com.mobilin.games;
 using UnityEngine;
 
 public class BikeUI : MonoBehaviour
 {
     private mvMotorcycleRiderShooter _mvMotorcycleRiderShooter;
+    public List<GameObject> disableInBikeMode = new List<GameObject>();
+    private Dictionary<GameObject, bool> originalStates = new Dictionary<GameObject, bool>();
 
     private void Start()
     {
@@ -13,10 +16,30 @@ public class BikeUI : MonoBehaviour
     public void MountBike()
     {
         _mvMotorcycleRiderShooter.EnterInput();
+        
+        // Store the original enabled state of each GameObject
+        originalStates.Clear();
+        foreach (var item in disableInBikeMode)
+        {
+            if (item != null)
+            {
+                originalStates[item] = item.activeSelf;
+                item.SetActive(false);
+            }
+        }
     }
 
     public void DisMount()
     {
         _mvMotorcycleRiderShooter.ExitInput();
+        
+        // Only re-enable GameObjects that were originally enabled
+        foreach (var item in disableInBikeMode)
+        {
+            if (item != null && originalStates.ContainsKey(item))
+            {
+                item.SetActive(originalStates[item]);
+            }
+        }
     }
 }
