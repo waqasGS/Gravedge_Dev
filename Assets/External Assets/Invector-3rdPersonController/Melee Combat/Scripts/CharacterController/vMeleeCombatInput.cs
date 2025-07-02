52b8417d961770cb389910f1f5fc78faed2f92ff
Umair Jamshaid <umair@good-games.net>
2025-07-02 16:59:35 +0500

    camera setting

--- Assets/External Assets/Invector-3rdPersonController/Melee Combat/Scripts/CharacterController/vMeleeCombatInput.cs
+++ Assets/External Assets/Invector-3rdPersonController/Melee Combat/Scripts/CharacterController/vMeleeCombatInput.cs
@@ -174,15 +174,24 @@ namespace Invector.vCharacterController
         /// <summary>
         /// Override the Sprint method to cancel Sprinting when Attacking
         /// </summary>
+        // public override void SprintInput()
+        // {
+        //     if (sprintInput.useInput)
+        //     {
+        //         bool canSprint = cc.useContinuousSprint ? sprintInput.GetButtonDown() : sprintInput.GetButton();
+        //         cc.Sprint(canSprint && !isAttacking);
+        //     }
+        // }
+
         public override void SprintInput()
         {
             if (sprintInput.useInput)
             {
-                bool canSprint = cc.useContinuousSprint ? sprintInput.GetButtonDown() : sprintInput.GetButton();
+                // Ab sirf button hold karne par sprint hogi
+                bool canSprint = sprintInput.GetButton();
                 cc.Sprint(canSprint && !isAttacking);
             }
         }
-
         #endregion
 
         #region Conditions
